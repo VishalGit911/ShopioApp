21bd129287095d669f6bc13e4d0cf8905cb8e1a8
VishalGit911 <vishalgit911@gmail.com>
2024-09-02 10:57:44 +0530

    02
    done

--- shoppio_app/lib/screen/onboarding/component/onboarding_body.dart
+++ shoppio_app/lib/screen/onboarding/component/onboarding_body.dart
@@ -45,37 +45,67 @@ class _OnBoardingBodyScreenState extends State<OnBoardingBodyScreen> {
               ),
             ),
             Expanded(
-                flex: 2,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    onBoardingContainer(selectindex),
-                    FloatingActionButton(
-                      backgroundColor: Colors.blue.shade800,
-                      foregroundColor: Colors.white,
-                      onPressed: () {
-                        setState(() {
-                          if (selectindex < onboardingdata.length - 1) {
-                            selectindex++;
-                            pageController.animateToPage(selectindex,
-                                duration: const Duration(milliseconds: 1500),
-                                curve: Curves.fastLinearToSlowEaseIn);
-                          } else {
-                            Navigator.pushNamedAndRemoveUntil(
-                              context,
-                              AppRoute.signin,
-                              (route) => false,
-                            );
-                          }
-                        });
-                      },
-                      child: const Icon(
-                        Icons.navigate_next,
-                        size: 40,
+                flex: 1,
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Container(
+                    height: 70,
+                    width: 300,
+                    decoration: BoxDecoration(
+                        color: Colors.blue.shade800,
+                        borderRadius: BorderRadius.circular(30)),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 20, right: 20),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            selectindex == 2 ? "Get Started" : "Next",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                if (selectindex < onboardingdata.length - 1) {
+                                  selectindex++;
+                                  pageController.animateToPage(selectindex,
+                                      duration:
+                                          const Duration(milliseconds: 1500),
+                                      curve: Curves.fastLinearToSlowEaseIn);
+                                } else {
+                                  Navigator.pushNamedAndRemoveUntil(
+                                    context,
+                                    AppRoute.signin,
+                                    (route) => false,
+                                  );
+                                }
+                              });
+                            },
+                            child: Container(
+                              height: 35,
+                              width: 60,
+                              decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(30)),
+                              child: Center(
+                                child: Icon(
+                                  Icons.navigate_next,
+                                  color: Colors.blue.shade800,
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
                       ),
-                    )
-                  ],
-                ))
+                    ),
+                  ),
+                )),
+            SizedBox(
+              height: 15,
+            )
           ],
         ),
       ),
